a45340b2d0d6c484327dd8b9c814b50d8aecbffe
foriamweak <foriamweak@gmail.com>
2020-08-23 20:33:54 +0900

    Add files via upload

---
+++ run_windowsEXE/loop1.bat
@@ -0,0 +1,38 @@
+
+
+for %%i IN (22, 27, 32, 37) do call percall_org.bat 	MarketPlace	1920x1080 %%i	60  1
+for %%i IN (22, 27, 32, 37) do call percall_org.bat 	RitualDance	1920x1080 %%i	60  1
+for %%i IN (22, 27, 32, 37) do call percall_org.bat 	Cactus		1920x1080 %%i	50  1
+for %%i IN (22, 27, 32, 37) do call percall_org.bat 	BasketballDrive	1920x1080 %%i	50  1
+for %%i IN (22, 27, 32, 37) do call percall_org.bat 	BQTerrace	1920x1080 %%i	60  1
+for %%i IN (22, 27, 32, 37) do call percall_org.bat 	BasketballDrill	832x480	%%i	50  1
+for %%i IN (22, 27, 32, 37) do call percall_org.bat 	BQMall		832x480	%%i	60  1
+for %%i IN (22, 27, 32, 37) do call percall_org.bat 	PartyScene	832x480	%%i	50  1
+for %%i IN (22, 27, 32, 37) do call percall_org.bat 	RaceHorsesC	832x480	%%i	30  1
+for %%i IN (22, 27, 32, 37) do call percall_org.bat 	BasketballPass	416x240	%%i	50  1
+for %%i IN (22, 27, 32, 37) do call percall_org.bat 	BQSquare	416x240	%%i	60  1
+for %%i IN (22, 27, 32, 37) do call percall_org.bat 	BlowingBubbles	416x240	%%i	50  1
+for %%i IN (22, 27, 32, 37) do call percall_org.bat 	RaceHorses	416x240	%%i	30  1
+for %%i IN (22, 27, 32, 37) do call percall_org.bat 	FourPeople	1280x720	%%i	60  1
+for %%i IN (22, 27, 32, 37) do call percall_org.bat 	Johnny		1280x720	%%i	60  1
+for %%i IN (22, 27, 32, 37) do call percall_org.bat 	KristenAndSara	1280x720	%%i	60  1
+
+
+for %%i IN (22, 27, 32, 37) do call percall_nn_y50.bat 	MarketPlace	1920x1080 %%i	60  1
+for %%i IN (22, 27, 32, 37) do call percall_nn_y50.bat 	RitualDance	1920x1080 %%i	60  1
+for %%i IN (22, 27, 32, 37) do call percall_nn_y50.bat 	Cactus		1920x1080 %%i	50  1
+for %%i IN (22, 27, 32, 37) do call percall_nn_y50.bat 	BasketballDrive	1920x1080 %%i	50  1
+for %%i IN (22, 27, 32, 37) do call percall_nn_y50.bat 	BQTerrace	1920x1080 %%i	60  1
+for %%i IN (22, 27, 32, 37) do call percall_nn_y50.bat 	BasketballDrill	832x480	%%i	50  1
+for %%i IN (22, 27, 32, 37) do call percall_nn_y50.bat 	BQMall		832x480	%%i	60  1
+for %%i IN (22, 27, 32, 37) do call percall_nn_y50.bat 	PartyScene	832x480	%%i	50  1
+for %%i IN (22, 27, 32, 37) do call percall_nn_y50.bat 	RaceHorsesC	832x480	%%i	30  1
+for %%i IN (22, 27, 32, 37) do call percall_nn_y50.bat 	BasketballPass	416x240	%%i	50  1
+for %%i IN (22, 27, 32, 37) do call percall_nn_y50.bat 	BQSquare	416x240	%%i	60  1
+for %%i IN (22, 27, 32, 37) do call percall_nn_y50.bat 	BlowingBubbles	416x240	%%i	50  1
+for %%i IN (22, 27, 32, 37) do call percall_nn_y50.bat 	RaceHorses	416x240	%%i	30  1
+for %%i IN (22, 27, 32, 37) do call percall_nn_y50.bat 	FourPeople	1280x720	%%i	60  1
+for %%i IN (22, 27, 32, 37) do call percall_nn_y50.bat 	Johnny		1280x720	%%i	60  1
+for %%i IN (22, 27, 32, 37) do call percall_nn_y50.bat 	KristenAndSara	1280x720	%%i	60  1
+
+pause
---
+++ run_windowsEXE/percall_nn_y125.bat
@@ -0,0 +1,25 @@
+@echo off
+set seqroot=C:\Sequence\
+if %4 == 24 (	
+	set frate=24
+) else if %4 == 30 (
+	set frate=32
+) else if %4 == 50 (
+	set frate=48
+) else if %4 == 60 (
+	set frate=64
+)
+set /a frameskip=%4 * 2
+if %5 == 1 (
+	if %4 == 60 (
+		set /a frm=%4 * 5 - %frameskip%
+	) else if %4 == 30 (
+		set /a frm=%4 * 10 - %frameskip%
+	) else if %4 == 50 (
+		set /a frm=%4 * 6 - %frameskip%
+	) 
+) else (
+	set /a frm=%4 * 10 - %frameskip%
+)
+@echo on
+EncoderApp4.0_NNh30y125.exe -c .\cfg\encoder_intra_vtm.cfg -c .\cfg\%1.cfg -q %3 -fs %frameskip% -f %frm% -b .\bin\%1_%3.bin -i %seqroot%%1_%2_%4.yuv >> .\dat\%1_%3.txt
